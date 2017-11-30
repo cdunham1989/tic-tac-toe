31e6ac457085f7d65a658fa80a23357233474603
Chris <cdunham1989@gmail.com>
2017-11-30 14:38:03 +0000

    finished building of player class, unit tests passing

--- spec/player_spec.js
+++ spec/player_spec.js
@@ -5,9 +5,16 @@ var Player = require("../src/player.js")
 describe('Player', function() {
   var player
 
-it('instantiaties with the name it is given as a parameter', function() {
-  player = new Player("Chris")
-  expect(player.getName()).toEqual("Chris");
-});
+  beforeEach(function() {
+    player = new Player("Chris", "O");
+  });
+
+  it('instantiaties with the name it is given as a parameter', function() {
+    expect(player.getName()).toEqual("Chris");
+  });
+
+  it('allows the symbol of the player to be set', function() {
+    expect(player.getSymbol()).toEqual("O");
+  });
 
 });
--- src/player.js
+++ src/player.js
@@ -1,12 +1,17 @@
 'use strict';
 
 class Player {
-  constructor(name){
+  constructor(name, symbol){
     this._name = name;
+    this._symbol = symbol;
   }
 
   getName() {
     return this._name;
+  }
+
+  getSymbol() {
+    return this._symbol;
   }
 }
 
